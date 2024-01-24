22cf2b897a3bbe6e349cef2b2eec672719376823
arthur <arthur@darkstar.example.net>
2024-01-24 23:53:38 +1300

    Updated system call

--- pucko-mail-openPart.pl
+++ pucko-mail-openPart.pl
@@ -10,16 +10,16 @@ if (not $path or not $part or not $mimeType) {
 
 $str = "path $path part $part mimeType $mimeType";
 if ($mimeType eq 'text/plain') {
-    system("pucko-extractMailPart $path $part >/tmp/pucko.txt");
+    system("pucko-mail-extractPart $path $part >/tmp/pucko.txt");
     system("chromium /tmp/pucko.txt");
 } elsif ($mimeType eq 'text/html') {
-    system("pucko-extractMailPart $path $part >/tmp/pucko.html");
+    system("pucko-mail-extractPart $path $part >/tmp/pucko.html");
     system("chromium /tmp/pucko.html");
 } elsif ($mimeType eq 'application/pdf') {
-    system("pucko-extractMailPart $path $part >/tmp/pucko.pdf");
+    system("pucko-mail-extractPart $path $part >/tmp/pucko.pdf");
     system("mupdf /tmp/pucko.pdf");
 } elsif ($mimeType eq 'image/png') {
-    system("pucko-extractMailPart $path $part >/tmp/pucko.png");
+    system("pucko-mail-extractPart $path $part >/tmp/pucko.png");
     system("chromium /tmp/pucko.png");
 } else {
     system("pucko", "showAlert:", "$path $part $mimeType");
